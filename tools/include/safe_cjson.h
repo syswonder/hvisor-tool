ea81ae3b57053f144e77cbeb158712d61c752568
Inquisitor-201 <1365605830@qq.com>
2025-03-26 17:06:10 +0800

    add safe_cjson functions for hvisor-tool

---
+++ tools/include/safe_cjson.h
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/**
+ * Copyright (c) 2025 Syswonder
+ *
+ * Syswonder Website:
+ *      https://www.syswonder.org
+ *
+ * Authors:
+ *      Guowei Li <2401213322@stu.pku.edu.cn>
+ */
+
+#include "cJSON.h"
+
+cJSON *__safe_cJSON_GetObjectItem(const char *filename, int line,
+                                  const cJSON *object, const char *key);
+int __safe_cJSON_GetArraySize(const char *filename, int line,
+                              const cJSON *array);
+cJSON *__safe_cJSON_GetArrayItem(const char *filename, int line,
+                                 const cJSON *array, int index);
+cJSON *__safe_cJSON_Parse(const char *filename, int line, const char *value);
+void __safe_cJSON_Delete(const char *filename, int line, cJSON *object);
+
+#define SAFE_CJSON_GET_OBJECT_ITEM(object, key)                                \
+    __safe_cJSON_GetObjectItem(__FILE__, __LINE__, object, key)
+#define SAFE_CJSON_GET_ARRAY_SIZE(array)                                       \
+    __safe_cJSON_GetArraySize(__FILE__, __LINE__, array)
+#define SAFE_CJSON_GET_ARRAY_ITEM(array, index)                                \
+    __safe_cJSON_GetArrayItem(__FILE__, __LINE__, array, index)
+#define SAFE_CJSON_PARSE(value) __safe_cJSON_Parse(__FILE__, __LINE__, value)
+#define SAFE_CJSON_DELETE(object)                                              \
+    __safe_cJSON_Delete(__FILE__, __LINE__, object)
---
+++ tools/safe_cjson.c
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/**
+ * Copyright (c) 2025 Syswonder
+ *
+ * Syswonder Website:
+ *      https://www.syswonder.org
+ *
+ * Authors:
+ *      Guowei Li <2401213322@stu.pku.edu.cn>
+ */
+
+#include "safe_cjson.h"
+#include "log.h"
+
+cJSON *__safe_cJSON_GetObjectItem(const char *filename, int line,
+                                  const cJSON *object, const char *key) {
+    if (!object) {
+        log_warn("%s:%d - [cJSON_GetObjectItem] JSON object is NULL", filename,
+                 line);
+        return NULL;
+    }
+    cJSON *item = cJSON_GetObjectItem(object, key);
+    if (!item) {
+        log_warn(
+            "%s:%d - [cJSON_GetObjectItem] Key '%s' not found in JSON object",
+            filename, line, key);
+    }
+    return item;
+}
+
+int __safe_cJSON_GetArraySize(const char *filename, int line,
+                              const cJSON *array) {
+    if (!array) {
+        log_warn("%s:%d - [cJSON_GetArraySize] Array is NULL", filename, line);
+        return 0;
+    }
+    return cJSON_GetArraySize(array);
+}
+
+cJSON *__safe_cJSON_GetArrayItem(const char *filename, int line,
+                                 const cJSON *array, int index) {
+    if (!array) {
+        log_warn("%s:%d - [cJSON_GetArrayItem] Array is NULL", filename, line);
+        return NULL;
+    }
+
+    const int size = cJSON_GetArraySize(array);
+    if (index < 0 || index >= size) {
+        log_warn(
+            "%s:%d - [cJSON_GetArrayItem] Index %d out of bounds (size: %d)",
+            filename, line, index, size);
+        return NULL;
+    }
+    return cJSON_GetArrayItem(array, index);
+}
+
+cJSON *__safe_cJSON_Parse(const char *filename, int line, const char *value) {
+    cJSON *obj = cJSON_Parse(value);
+    if (!obj) {
+        log_warn("%s:%d - [cJSON_Parse] Failed to parse JSON file", filename,
+                 line);
+    }
+    return obj;
+}
+
+void __safe_cJSON_Delete(const char *filename, int line, cJSON *object) {
+    if (!object) {
+        log_warn("%s:%d - [cJSON_Delete] Delete NULL object", filename, line);
+        return;
+    }
+    cJSON_Delete(object);
+}
