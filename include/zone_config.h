2a6c4a5b4c7dd9e113f78774d840f67f13704f06
hangqi-ren <2572131118@qq.com>
2025-09-07 17:23:24 +0800

    Synchronize changes to the GIC parameter structure

--- include/zone_config.h
+++ include/zone_config.h
@@ -52,21 +52,63 @@ struct pci_config {
 typedef struct pci_config pci_config_t;
 
 #ifdef ARM64
-struct arch_zone_config {
+// struct arch_zone_config {
+//     __u64 gicd_base;
+//     __u64 gicd_size;
+//     __u64 gicr_base;
+//     __u64 gicr_size;
+//     __u64 gits_base;
+//     __u64 gits_size;
+//     __u64 gicc_base;
+//     __u64 gicc_offset;
+//     __u64 gicc_size;
+//     __u64 gich_base;
+//     __u64 gich_size;
+//     __u64 gicv_base;
+//     __u64 gicv_size;
+// };
+
+// enum class GicVesionTag: __u64 { v2, v3 };
+
+struct Gicv2Config{
+    __u64 gicd_base;
+    __u64 gicd_size;
+    __u64 gicc_base;
+    __u64 gicc_size;
+    __u64 gicc_offset;
+    __u64 gich_base;
+    __u64 gich_size;
+    __u64 gicv_base;
+    __u64 gicv_size; 
+};
+
+struct Gicv3Config{
     __u64 gicd_base;
     __u64 gicd_size;
     __u64 gicr_base;
     __u64 gicr_size;
     __u64 gits_base;
     __u64 gits_size;
-    __u64 gicc_base;
-    __u64 gicc_offset;
-    __u64 gicc_size;
-    __u64 gich_base;
-    __u64 gich_size;
-    __u64 gicv_base;
-    __u64 gicv_size;
+};
+
+struct Gicv2Payload {
+    __u64 gic_version_tag;
+    struct Gicv2Config gicv2_config;
+};
+
+struct Gicv3Payload {
+    __u64 gic_version_tag;
+    struct Gicv3Config gicv3_config;
+};
+
+union GicConfig {
+    struct Gicv2Payload gicv2;
+    struct Gicv3Payload gicv3;
+};
+
+struct arch_zone_config {
     __u8 is_aarch32;
+    union GicConfig gic_config; 
 };
 #endif
 
@@ -99,7 +141,7 @@ struct ivc_config {
 };
 typedef struct ivc_config ivc_config_t;
 
-#define CONFIG_MAGIC_VERSION 0x02
+#define CONFIG_MAGIC_VERSION 0x03
 
 // Every time you change the struct, you should also change the
 // `CONFIG_MAGIC_VERSION`
